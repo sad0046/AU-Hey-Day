fbe98bbebbdf5879dc2ca980b68f195e559103aa
sad0046 <114424426+sad0046@users.noreply.github.com>
2022-10-10 12:09:46 -0500

    Add files via upload
    Upload Donohoo Hey Day Script

---
+++ Donohoo_HeyDay.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+# Hey Day Script
+
+# Using echo have the user enter their name.
+echo "Enter your name"
+
+# Read will assign the user name to the variable name.
+read Name 
+
+#print statement to user
+echo "Hey, $Name! Welcome to Auburn University Hey Day!"
